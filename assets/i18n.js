0ad0b9195e983de270727eedf23358190b3b9216
Andreas Krings <sugikei0131@gmail.com>
2026-08-08 01:37:39 +0200

    Clean up: remove test work-13, fix duplicates, restore 12 works

--- assets/i18n.js
+++ assets/i18n.js
@@ -38,8 +38,6 @@
     'work.title.11': 'Die Treppe des Meeres',
     'work.title.12': 'Der Spiegel des Ozeans',
 
-    // work titles 13+ (added via dashboard)
-    'work.title.13': 'Die Wolke, die zu regnen vergass',
 
     // exhibition names
     'ex.1': 'Das Gewicht des Schwebens',
